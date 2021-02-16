caf1637eb25bb94cb544c83cb169ebceda0a2a2c
Christiaan van de Sande <christiaanvandesande@Christiaans-MacBook-Air.local>
2021-02-16 11:54:30 -0700

    removed unused references

--- GraphPane.java
+++ GraphPane.java
@@ -1,12 +1,3 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.*;
-import javafx.scene.control.*;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-
 public class GraphPane {
   private Graph graph;
   
