b8013016d2a87e055602977501aab6fa7bf3484d
Christiaan van de Sande <christiaanvandesande@Christiaans-MacBook-Air.local>
2021-04-29 22:32:01 -0700

    added canvas resizing

--- DrawingPane.java
+++ DrawingPane.java
@@ -118,4 +118,35 @@ public class DrawingPane extends Canvas implements Listener {
       dragVertex = null;
     }
   }   
+
+  @Override
+  public boolean isResizable() {
+    return true;
+  }
+
+  @Override
+  public double maxHeight(double width) {
+    return Double.POSITIVE_INFINITY;
+  }
+
+  @Override
+  public double maxWidth(double height) {
+    return Double.POSITIVE_INFINITY;
+  }
+
+  @Override
+  public double minWidth(double height) {
+    return 1D;
+  }
+
+  @Override
+  public double minHeight(double width) {
+    return 1D;
+  }
+
+  @Override
+  public void resize(double width, double height) {
+    this.setWidth(width);
+    this.setHeight(height);
+  }
 }
